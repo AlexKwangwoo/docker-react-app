d026360644da7e4fb4f64f316c491d8bf97a8c4e
Kwangwoo <bnc3049@gmail.com>
2021-04-25 14:16:09 -0600

    Added travis 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello!!!!!aaahahahaaza
+          learn react
         </a>
       </header>
     </div>
